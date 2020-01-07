ed6ab8a2670bb0f5ac18e9c71bcc5dd6d162e52d
Yuqi-cuhk <39298111+Yuqi-cuhk@users.noreply.github.com>
2020-01-07 15:03:23 +0800

    Update unet_parts.py
    predict the boundary of a predicted area.

--- step3_combine_area_bd/unet/unet_parts.py
+++ step3_combine_area_bd/unet/unet_parts.py
@@ -1,64 +1,30 @@
-from __future__ import print_function, division
 import torch
 import torch.nn as nn
-from torch.nn.parameter import Parameter
-import numpy as np
 
 
-class conv_sknet(nn.Module):
-    def __init__(self, in_ch, out_ch, reduction=16):
-        super(conv_sknet, self).__init__()
-
-        def register(name, tensor):
-            self.register_parameter(name, Parameter(tensor))
-
-        register('A', torch.rand(max(out_ch // reduction, 32), out_ch))
-        register('B', torch.rand(max(out_ch // reduction, 32), out_ch))
-
-        self.conv_3 = nn.Sequential(
+class Double_conv(nn.Module):
+    # (conv => BN => ReLU) * 2
+    def __init__(self, in_ch, out_ch):
+        super(Double_conv, self).__init__()
+        self.conv = nn.Sequential(
             nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            # default: nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True)
             nn.BatchNorm2d(out_ch, track_running_stats=False),
             nn.ReLU(inplace=True),
-        )
-
-        self.conv_5 = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, 3, dilation=2, padding=2),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
             nn.BatchNorm2d(out_ch, track_running_stats=False),
-            nn.ReLU(inplace=True),
-        )
-
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-
-        self.fc = nn.Sequential(
-                nn.Linear(out_ch, max(out_ch // reduction, 32)),
-                nn.ReLU(inplace=True),
+            nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
-        x_3 = self.conv_3(x)
-        x_5 = self.conv_5(x)
-        x_fuse = x_3 + x_5
-        b, c, _, _ = x_fuse.size()  # b=4, c=128
-        x_fuse_s = self.avg_pool(x_fuse).view(b, c)
-        x_fuse_z = self.fc(x_fuse_s)
-
-        s1 = torch.Tensor(np.exp(np.array(torch.mm(x_fuse_z, self.A).cpu().detach().numpy())) / (np.exp(np.array(torch.mm(x_fuse_z, self.A).cpu().detach().numpy())) + np.exp(np.array(torch.mm(x_fuse_z, self.B).cpu().detach().numpy()))))
-        s1 = s1.view(b, c, 1, 1).cuda()
-        s2 = 1 - s1
-        V_a = x_3 * s1
-        V_b = x_5 * s2
-        V = V_a + V_b
-
-        return V
+        x = self.conv(x)
+        return x
 
 
 class Inconv(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(Inconv, self).__init__()
-        self.conv = nn.Sequential(
-            conv_sknet(in_ch, out_ch),
-            conv_sknet(out_ch, out_ch),
-        )
+        self.conv = Double_conv(in_ch, out_ch)
 
     def forward(self, x):
         x = self.conv(x)
@@ -70,8 +36,7 @@ class Down(nn.Module):
         super(Down, self).__init__()
         self.mpconv = nn.Sequential(
             nn.MaxPool2d(2),
-            conv_sknet(in_ch, out_ch),
-            conv_sknet(out_ch, out_ch),
+            Double_conv(in_ch, out_ch)
         )
 
     def forward(self, x):
@@ -84,15 +49,26 @@ class Up1(nn.Module):
         super(Up1, self).__init__()
 
         self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.conv1 = conv_sknet(2 * in_ch, in_ch)
-        self.conv2 = conv_sknet(in_ch, out_ch)
-
+        self.conv = Double_conv(2 * in_ch, out_ch)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
         x = torch.cat([x2, x1], dim=1)  #  order matters?
-        x = self.conv1(x)
-        x = self.conv2(x)
+        x = self.conv(x)
+        return x
+
+
+class Up_cons(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(Up_cons, self).__init__()
+
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.conv = Double_conv(2 * in_ch, out_ch)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
         return x
 
 
@@ -101,15 +77,13 @@ class Up2(nn.Module):
         super(Up2, self).__init__()
 
         self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.conv1 = conv_sknet(1024, 2 * out_ch)
-        self.conv2 = conv_sknet(2 * out_ch, out_ch)
+        self.conv = Double_conv(1024, out_ch)
 
     def forward(self, x1, x2, x3):
         x1 = self.up(x1)
         x3 = nn.functional.interpolate(x3, scale_factor=2, mode='bilinear', align_corners=True)
-        x = torch.cat([x3, x2, x1], dim=1)
-        x = self.conv1(x)
-        x = self.conv2(x)
+        x = torch.cat([x3, x2, x1], dim=1)  #  order matters?
+        x = self.conv(x)
         return x
 
 
@@ -118,15 +92,13 @@ class Up3(nn.Module):
         super(Up3, self).__init__()
 
         self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.conv1 = conv_sknet(512, 2 * out_ch)
-        self.conv2 = conv_sknet(2 * out_ch, out_ch)
+        self.conv = Double_conv(512, out_ch)
 
     def forward(self, x1, x2, x3):
         x1 = self.up(x1)
         x2 = nn.functional.interpolate(x2, scale_factor=2, mode='bilinear', align_corners=True)
         x = torch.cat([x1, x3, x2], dim=1)  # order matters?
-        x = self.conv1(x)
-        x = self.conv2(x)
+        x = self.conv(x)
         return x
 
 
@@ -135,15 +107,13 @@ class Up4(nn.Module):
         super(Up4, self).__init__()
 
         self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.conv1 = conv_sknet(256, out_ch)
-        self.conv2 = conv_sknet(out_ch, out_ch)
+        self.conv = Double_conv(256, out_ch)
 
     def forward(self, x1, x2, x3):
         x1 = self.up(x1)
         x3 = nn.functional.interpolate(x3, scale_factor=2, mode='bilinear', align_corners=True)
         x = torch.cat([x1, x2, x3], dim=1)
-        x = self.conv1(x)
-        x = self.conv2(x)
+        x = self.conv(x)
         return x
 
 
@@ -155,4 +125,3 @@ class Outconv(nn.Module):
     def forward(self, x):
         x = self.conv(x)
         return x
-
